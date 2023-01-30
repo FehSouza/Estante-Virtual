a5e79c963b191aea659bf41a2fdb53fda82d2e04
FehSouza <engcivil.fernandasouza@gmail.com>
2023-01-30 19:10:21 -0300

    feat(not-found): page not found create

--- src/pages/Not Found/index.tsx
+++ src/pages/Not Found/index.tsx
@@ -1,5 +1,14 @@
 import * as S from './styles'
 
 export const NotFound = () => {
-  return <S.NotFound>Página não encontrada</S.NotFound>
+  return (
+    <S.NotFound>
+      <S.Title>Oops, sentimos muito : (</S.Title>
+      <S.Subtitle>Esta página não foi encontrada</S.Subtitle>
+
+      <S.Error>404</S.Error>
+
+      <S.Button to="/">Ir para a Home</S.Button>
+    </S.NotFound>
+  )
 }
--- src/pages/Not Found/styles.ts
+++ src/pages/Not Found/styles.ts
@@ -1,9 +1,49 @@
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
 export const NotFound = styled.h1`
-  min-height: 1440px;
   max-width: 1440px;
   width: calc(100% - 32px);
-  padding: 16px;
+  padding: 0 16px;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const Title = styled.h2`
+  margin-top: 68px;
+  font-size: 34px;
+  line-height: 36px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.text};
+`
+
+export const Subtitle = styled.span`
+  margin-top: 20px;
+  font-size: 34px;
+  line-height: 36px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.text};
+`
+
+export const Error = styled.div`
+  margin: 208px 0;
+  font-size: 56px;
+  line-height: 58px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.text};
+`
+
+export const Button = styled(NavLink)`
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.white};
+  padding: 24px 32px;
+  border-radius: 28px;
+  width: fit-content;
+  transition-duration: 350ms;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.secondary};
+  }
 `
